6ca12820578cb6714ba624eb85375429be6756cc
matanya alkobi <matanor123456@gmail.com>
2025-09-01 23:26:17 +0300

    Added a button that, when clicked, resets the votes.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import VoteOptions from "./components/VoteOptions";
 
 function App() {
-  const [numberOfVotes, setNumberOfVotes] = useState<{ [key: string]: number }>({
-    React: 0,
-    Vue: 0,
-    Svelte: 0,
-  });
+  const [numberOfVotes, setNumberOfVotes] = useState<{ [key: string]: number }>(
+    {
+      React: 0,
+      Vue: 0,
+      Svelte: 0,
+    }
+  );
   const [showResults, setShowResults] = useState<boolean>(false);
 
   const handleVote = (option: string) => {
@@ -21,14 +23,33 @@ function App() {
     <>
       <section className="app-container">
         <h1>Vote for your favorite framework!</h1>
-        <VoteOptions numberOfVotes={numberOfVotes} showResults={showResults} fn={handleVote}/>
-
+        <VoteOptions
+          numberOfVotes={numberOfVotes}
+          showResults={showResults}
+          fn={handleVote}
+        />
       </section>
 
+      <section className="container-functional-buttons">
 
-      <button className="toggle-button" onClick={() => setShowResults((prev) => !prev)}>
-        {showResults ? "Hide Results" : "Show Results"}
-      </button>
+        <button
+          className="functional-buttons"
+          onClick={() => setShowResults((prev) => !prev)}
+        >
+          {showResults ? "Hide Results" : "Show Results"}
+        </button>
+        <button className="functional-buttons"
+          onClick={() => {
+            setNumberOfVotes({
+              React: 0,
+              Vue: 0,
+              Svelte: 0,
+            });
+          }}
+        >
+          reset
+        </button>
+      </section>
     </>
   );
 }
